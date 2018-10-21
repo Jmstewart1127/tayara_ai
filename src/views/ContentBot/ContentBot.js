61bb9bfa03e414eb2e9347a3d6fa7b9d3e42c322
Fouad Wahabi <fouad.wahabi@gmail.com>
2018-10-20 20:34:06 -0500

    Fix port

--- src/views/ContentBot/ContentBot.js
+++ src/views/ContentBot/ContentBot.js
@@ -92,7 +92,7 @@ class ContentBot extends Component {
                                 <p className="App-intro">
                                     Upload picture.
                                 </p>
-                                <FilePond name="image" server="http://137.74.165.25:9402/upload"/>
+                                <FilePond name="image" server="http://137.74.165.25:4902/upload"/>
                             </Col>
                         </Row>
                     </CardBody>
